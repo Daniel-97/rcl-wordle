faba7b0cb3ff0866525c82ee34c4ab1bb2cb2826
daniele <zeolla.d@gmail.com>
2023-10-10 19:38:38 +0200

    Aggiunte istruzioni a readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # rcl-wordle
 Progetto laboratorio reti di calcolatori Unipi
+
+# BUILD
+mvn clean install
+
+# RUN
+WORDLE_CONFIG=server.config java -jar target/Server-jar-with-dependencies.jar
+WORDLE_CONFIG=client.config java -jar target/Client-jar-with-dependencies.jar
+
--- build.sh
+++ build.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
+# MEAVEN
+mvn clean install
 
-# Compile server
-javac -d target -sourcepath src -cp src/resources/gson-2.10.1.jar src/server/ServerMain.java
-# Compile client
-javac -d target -sourcepath src -cp src/resources/gson-2.10.1.jar src/client/ClientMain.java
+#echo "Avvio il server..."
+#WORDLE_CONFIG=server.config java -jar target/Server-jar-with-dependencies.jar > server.log 2>&1 &
+#echo "Server avviato! pid: $$. Output su server.log"
 
-# Run server
-#WORDLE_CONFIG=server.config java -cp src/resources/gson-2.10.1.jar:target server.ServerMain server.config
-
-# Run client
-#WORDLE_CONFIG=client.config java -cp src/resources/gson-2.10.1.jar:target client.ClientMain client.config
-
-jar -cvfe Server.jar target/server/ServerMain target # funziona
+#echo "Avvio un'istanza del client..."
+#java -jar target/Client-jar-with-dependencies.jar
