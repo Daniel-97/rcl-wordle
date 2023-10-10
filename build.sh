dd78a3db57a4366fff9a3aee813c888ed3e9ad15
daniele <zeolla.d@gmail.com>
2023-10-10 17:42:30 +0200

    Aggiunto script di compilazione del codice

--- build.sh
+++ build.sh
@@ -9,5 +9,6 @@ javac -d target -sourcepath src -cp src/resources/gson-2.10.1.jar src/client/Cli
 #WORDLE_CONFIG=server.config java -cp src/resources/gson-2.10.1.jar:target server.ServerMain server.config
 
 # Run client
-#EORDLE_CONFIG=client.config java -cp src/resources/gson-2.10.1.jar:target client.ClientMain client.config
+#WORDLE_CONFIG=client.config java -cp src/resources/gson-2.10.1.jar:target client.ClientMain client.config
 
+jar -cvfe Server.jar target/server/ServerMain target # funziona
