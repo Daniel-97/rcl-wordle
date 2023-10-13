5ee6abefe758797ff669f64c10a2e1700dedac78
daniele <zeolla.d@gmail.com>
2023-10-13 21:32:54 +0200

    Cambiati colori log a bold

--- src/main/java/common/utils/WordleLogger.java
+++ src/main/java/common/utils/WordleLogger.java
@@ -7,13 +7,12 @@ import java.util.Date;
 public class WordleLogger {
 
 	private static final String ANSI_RESET = "\u001B[0m";
-	private static final String ANSI_RED = "\u001B[31m";
-	private static final String ANSI_GREEN = "\u001B[32m";
-	private static final String ANSI_YELLOW = "\u001B[33m";
-	private static final String ANSI_BLUE = "\u001B[34m";
-	private static final String ANSI_WHITE = "\u001B[37m";
-	public static final String PURPLE = "\033[0;35m";
-	public static final String CYAN = "\033[0;36m";
+	public static final String WHITE_BOLD = "\033[1;37m";
+	public static final String CYAN_BOLD = "\033[1;36m";
+	public static final String BLUE_BOLD = "\033[1;34m";
+	public static final String RED_BOLD = "\033[1;31m";
+	public static final String GREEN_BOLD = "\033[1;32m";
+	public static final String YELLOW_BOLD = "\033[1;33m";
 
 	private String className;
 	public WordleLogger(String className) {
@@ -26,31 +25,31 @@ public class WordleLogger {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 		System.out.println(
 				"["+color + level + ANSI_RESET + "]" +
-				"["+ ANSI_WHITE + Thread.currentThread().getName() + ANSI_RESET + "]" +
-				"["+ CYAN + className + ANSI_RESET + "]" +
-				"["+ ANSI_WHITE + dateFormat.format(new Date()) + ANSI_RESET + "] " +
+				"["+ WHITE_BOLD + Thread.currentThread().getName() + ANSI_RESET + "]" +
+				"["+ CYAN_BOLD + className + ANSI_RESET + "]" +
+				"["+ WHITE_BOLD + dateFormat.format(new Date()) + ANSI_RESET + "] " +
 				message + "\n");
 	}
 
 	public void info(String message) {
-		this.log(ANSI_WHITE, "INFO", message);
+		this.log(WHITE_BOLD, "INFO", message);
 	}
 
 	public void debug(String message) {
 		if (System.getenv("WORDLE_DEBUG") != null) {
-			this.log(ANSI_BLUE,"DEBUG", message);
+			this.log(BLUE_BOLD,"DEBUG", message);
 		}
 	}
 
 	public void error(String message) {
-		this.log(ANSI_RED,"ERROR", message);
+		this.log(RED_BOLD,"ERROR", message);
 	}
 
 	public void warn(String message) {
-		this.log(ANSI_YELLOW,"WARNING", message);
+		this.log(YELLOW_BOLD,"WARNING", message);
 	}
 
 	public void success(String message) {
-		this.log(ANSI_GREEN, "SUCCESS", message);
+		this.log(GREEN_BOLD, "SUCCESS", message);
 	}
 }
